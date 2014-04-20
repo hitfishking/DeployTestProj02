26feacb6c5d5486194745bf5f8a3c363f4022e2d
hitfishking <handinmine@126.com>
2014-04-20 20:44:33 +0800

    222

---
+++ config/deploy.rb
@@ -0,0 +1,41 @@
+set :application, "DeployTestingProject"
+set :repository,  "git@github.com:hitfishking/DeployTestProj02.git"
+set :deploy_to, "/opt/nginx/html/rails_apps/DeployTestProj02"
+set :scm, :git
+set :branch, "master"
+set :user, "hitfishking"
+set :use_sudo, false
+set :rails_env, "production"
+set :deploy_via, :copy
+set :ssh_options, { :forward_agent => true, :port => 4321 }
+ssh_options[:user] = "hitfishking"
+ssh_options[:port] = 4321
+ssh_options[:keys] = [File.join(ENV["HOME"], ".ssh", "id_rsa")]
+
+set :keep_releases, 5
+default_run_options[:pty] = true
+
+server "115.28.43.56", :app, :web, :db, :primary => true
+
+# set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+
+#role :web, "your web-server here"                          # Your HTTP server, Apache/etc
+#role :app, "your app-server here"                          # This may be the same as your `Web` server
+#role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
+#role :db,  "your slave db-server here"
+
+# if you want to clean up old releases on each deploy uncomment this:
+# after "deploy:restart", "deploy:cleanup"
+
+# if you're still using the script/reaper helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+# If you are using Passenger mod_rails uncomment this:
+# namespace :deploy do
+#   task :start do ; end
+#   task :stop do ; end
+#   task :restart, :roles => :app, :except => { :no_release => true } do
+#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+#   end
+# end
